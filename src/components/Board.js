c10866ed1876d94a8d496107c0b0a00894525b6e
lanegrimaud <lanewgrimaud@gmail.com>
2021-06-05 14:16:57 -0400

    board pages now display child columns and todos

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,53 @@
 import React, { useState, useEffect } from 'react'
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 
 export default function Board() {
     const { id } = useParams()
-    const [boardID, setBoardID] = useState(null)
+    const [board, setBoard] = useState({ columns: [] })
+
+    function getBoard(givenID) {
+        axios
+            .get(`https://managetheday-api.herokuapp.com/boards/${givenID}`)
+            .then(
+                (response) => {
+                    console.log(response.data.board)
+                    setBoard(response.data.board)
+                },
+                (err) => console.error(err)
+            )
+            .catch((error) => console.error(error));
+    };
 
     useEffect(() => {
         fetch(`http://localhost:5000/boards/${id}`)
-            .then(setBoardID(id))
+            .then(
+                getBoard(id)
+            )
     }, [id])
 
-
     return (
-        <div>
-            <h1>{boardID}</h1>
-        </div>
+        <>
+            <h1>{board.title}</h1>
+            {board.columns.map((column) => {
+                return (
+                    <Card key={column.id}>
+                        <Card.Header>{column.title}</Card.Header>
+                        <Card.Body>
+                            <ListGroup variant='flush'>
+                                {column.todos.map((todo) => {
+                                    return (
+                                        <ListGroupItem>{todo.task}</ListGroupItem>
+                                    )
+                                })}
+
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+        </>
     )
 }
