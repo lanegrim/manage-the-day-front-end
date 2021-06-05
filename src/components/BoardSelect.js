0f18acfc4e0ae49c33ba124ed1b83cecc1b6274d
lanegrimaud <lanewgrimaud@gmail.com>
2021-06-05 13:18:18 -0400

    users can now name and create new boards

--- src/components/BoardSelect.js
+++ src/components/BoardSelect.js
@@ -1,10 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { Card, Container, ListGroup, ListGroupItem } from 'react-bootstrap'
+import React, { useState, useEffect, useRef } from 'react'
+import { Card, Container, ListGroup, ListGroupItem, Button, Alert, Form } from 'react-bootstrap'
 import axios from "axios";
+import { Link, useHistory } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function BoardSelect() {
 
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+    const boardNameRef = useRef()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
     const [boards, setBoards] = useState([])
+
+
+    const linkStyle = {
+        textDecoration: 'none',
+        color: 'white'
+    }
+
+    async function handleLogout() {
+        setError('')
+
+        try {
+            await logout()
+            history.pushState('/login')
+        } catch {
+            setError('Failed to log out')
+        }
+    }
 
     function getBoards() {
         axios
@@ -13,11 +37,30 @@ export default function BoardSelect() {
                 (response) => {
                     setBoards(response.data.boards)
                     console.log(response.data.boards)
+                    console.log(currentUser.email)
                 },
                 (err) => console.error(err)
             )
             .catch((error) => console.error(error));
     };
+
+    function addBoard(event) {
+        event.preventDefault()
+        setLoading(true)
+        const newBoard = {
+            title: boardNameRef.current.value,
+            owner: currentUser.email
+        }
+        axios.post('https://managetheday-api.herokuapp.com/boards', newBoard)
+            .then((response) => {
+                getBoards()
+                setLoading(false)
+            },
+                (err) => console.error(err)
+            )
+            .catch((error) => console.error(error));
+    }
+
 
     useEffect(() => {
         getBoards()
@@ -28,13 +71,36 @@ export default function BoardSelect() {
             <Card>
                 <Card.Header>Select a Board</Card.Header>
                 <Card.Body>
-                    <ListGroup variant="flush">
+                    <ListGroup variant='flush'>
                         {boards.map((board) => {
-                            return <ListGroupItem key={board.id}>{board.title}</ListGroupItem>
+                            if (board.owner === currentUser.email) {
+                                return <ListGroupItem key={board.id}>
+                                    <Button className='w-100'>
+                                        <Link to={{ pathname: `/boards/${board.id}` }} style={linkStyle}>
+                                            <strong>{board.title}</strong>
+                                        </Link>
+                                    </Button>
+                                </ListGroupItem>
+                            }
                         })}
+                        <ListGroupItem>
+                            <form onSubmit={addBoard}>
+                                <Form.Group id="boardName">
+                                    <Form.Label>Or Add a New Board</Form.Label>
+                                    <Form.Control type="text" ref={boardNameRef} required placeholder='Name Your New Board' />
+                                </Form.Group>
+                                <Button className="w-100 mt-2" type="submit">
+                                    Create Board
+                                </Button>
+                            </form>
+                        </ListGroupItem>
                     </ListGroup>
                 </Card.Body>
             </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
+                {error && <Alert variant="danger">{error}</Alert>}
+            </div>
         </Container>
     )
 }
